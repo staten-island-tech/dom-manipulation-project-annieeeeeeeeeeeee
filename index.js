34526454959f4ef96aa4074689d69bc49604ed11
annieeeeeeeeeeeee <annieou000@gmail.com>
2022-11-02 10:16:41 -0400

    2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("connected");
+const DOMSelectors = {
+  input: document.querySelectorAll(".form"),
+  button: document.querySelectorAll(".btn"),
+  background: document.getElementById("#background"),
+};
+
+console.log(DOMSelectors);
+
+function test(background) {
+  background.style.backgroundColor = "red";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  test(DOMSelectors.background);
+});
