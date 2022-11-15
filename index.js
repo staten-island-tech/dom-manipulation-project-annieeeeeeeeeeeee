ce72a345ec013660b076df7a753f77a941eab8de
annieeeeeeeeeeeee <annieou000@gmail.com>
2022-11-15 10:16:18 -0500

    5

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-console.log("connected");
-
 const DOMSelectors = {
   form: document.getElementById("form"),
   button: document.querySelector(".btn"),
-  heading: document.getElementById("heading"),
-
-  //paragraph: document.getElementById("#text"),
+  input: document.querySelectorAll(".textbox"),
+  displaySection: document.getElementById("display"),
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event);
+  let albumName = document.getElementById("title").value;
+  console.log(albumName);
+
+  let artistName = document.getElementById("artist").value;
+  console.log(artistName);
+
+  let img = document.getElementById("url").value;
+  console.log(img);
+
+  DOMSelectors.displaySection.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="display-card">
+    <img
+      class="display-img"
+      src=${img}"
+    />
+    <h2 class="display-artist">${artistName}</h2>
+    <h3 class="display-album">${albumName}</h3>
+    <button class="remove btn">Remove Album</button>
+  </div> `
+  );
 });
-
-function display() {
-  DOMSelectors.
-}
-const albumName = document.getElementById("title").value;
-const artistName = document.getElementById("artist").value;
-const img = document.getElementById("url").value;
-
-console.log(albumName);
