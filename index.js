39c502b4d19365af173c052aff4c23a54408dc8c
annieeeeeeeeeeeee <annieou000@gmail.com>
2022-11-16 10:15:54 -0500

    6

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const DOMSelectors = {
   displaySection: document.getElementById("display"),
 };
 
+console.log(DOMSelectors.removeButton);
+
+function clearFields() {
+  document.getElementById("title").value = "";
+  document.getElementById("artist").value = "";
+  document.getElementById("url").value = "";
+}
+
+function deleteAlbum(target) {
+  if (target.className === "remove btn") {
+    target.parentElement.remove();
+  }
+}
+
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   let albumName = document.getElementById("title").value;
@@ -21,11 +35,19 @@ DOMSelectors.form.addEventListener("submit", function (event) {
     `<div class="display-card">
     <img
       class="display-img"
-      src=${img}"
+      src=${img}
     />
-    <h2 class="display-artist">${artistName}</h2>
     <h3 class="display-album">${albumName}</h3>
+    <h2 class="display-artist">${artistName}</h2>
+    
     <button class="remove btn">Remove Album</button>
   </div> `
   );
+
+  clearFields();
+});
+
+DOMSelectors.displaySection.addEventListener("click", function (event) {
+  deleteAlbum(event.target);
+  event.preventDefault();
 });
